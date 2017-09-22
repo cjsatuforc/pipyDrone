8c438951ee6f120e68987cade0304a3039874ba0
michaelymo <dynajyobi@gmail.com>
2017-09-22 07:14:00 +0000

    add first file

---
+++ moveServo/servo_motor.py
@@ -0,0 +1,28 @@
+import wiringpi
+import time
+import sys
+
+servo_pin  =  18
+
+# get degree
+param = sys.argv
+set_degree = int(param[1])
+print(set_degree)
+
+
+wiringpi.wiringPiSetupGpio()
+# input / output
+wiringpi.pinMode( servo_pin, 2 )
+
+
+wiringpi.pwmSetMode(0)
+wiringpi.pwmSetRange(1024)
+wiringpi.pwmSetClock(375)
+    
+wiringpi.pwmWrite( servo_pin, set_degree )
+
+if ( set_degree >= -90 ):
+    move_deg = int( 81 + 41 / 90 * set_degree )
+    wiringpi.pwmWrite( servo_pin, move_deg )
+
+
